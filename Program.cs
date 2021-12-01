b0ce0bc0a3def11a3b72d4bcfb2b0d9d24563ae9
Sena Özkara <75096851+Sena-Ozkara@users.noreply.github.com>
2021-12-01 18:46:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,77 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace levenshtein_çalışma
+namespace UsingLevenshtein
 {
     class Program
-
     {
         static void Main(string[] args)
-        { 
-        
-                Console.WriteLine(StringDistance.LevenshteinDistance("climax", "volmax"));
-                Console.WriteLine(StringDistance.LevenshteinDistance("Ram", "Raman"));
-                Console.WriteLine(StringDistance.LevenshteinDistance("Mama", "Mom"));
-                Console.ReadLine();
-        
-        }
-            public static class StringDistance
         {
-            /// <summary>
-            /// Compute the distance between two strings.
-            /// </summary>
-            public static int LevenshteinDistance(string s, string t)
-            {
-                int n = s.Length;
-                int m = t.Length;
-                int[,] d = new int[n + 1, m + 1];
-
-                // Step 1
-                if (n == 0)
-                {
-                    return m;
-                }
-
-                if (m == 0)
-                {
-                    return n;
-                }
-
-                // Step 2
-                for (int i = 0; i <= n; d[i, 0] = i++)
-                {
-                }
-
-                for (int j = 0; j <= m; d[0, j] = j++)
-                {
-                }
-
-                // Step 3
-                for (int i = 1; i <= n; i++)
-                {
-                    //Step 4
-                    for (int j = 1; j <= m; j++)
-                    {
-                        // Step 5
-                        int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-
-                        // Step 6
-                        d[i, j] = Math.Min(
-                            Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                            d[i - 1, j - 1] + cost);
-                    }
-                }
-                // Step 7
-                return d[n, m];
-            }
+            TestMethod("sena", "asena");
+            TestMethod("sam", "samantha");
+            TestMethod("mike", "mayk");
+            TestMethod("ozkra", "özkara");//Örnekleri istediğiniz gibi çoğaltabilirsiniz.
+            Console.Read(); // Çıktıyı görebilmek için yazıyoruz.Bekleyip kapanmaması için.
         }
 
-       
+        private static void TestMethod(string Source, string Target)
+        {
+            int[,] matrix3 = new int[Source.Length, Target.Length];
+            int distance3 = Source.FindLevenshteinDistance(Target, out matrix3);
+            Console.WriteLine("{0} ve {1}\nDistance : {2}\n", Source, Target, distance3);
+            WriteToConsole(matrix3);
+          
+        }
+        static void WriteToConsole(int[,] Matrix)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write("\t{0}  ", Matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(); 
+            
+        }
+                
     }
 
-}   
-  
+    public static class StringExtensions
+    {
+        // Genişletme metodu, karşılaştırma matrisini de out parametresi olarak döndürmektedir. 
+        public static int FindLevenshteinDistance(this string Source, string Target, out int[,] Matrix)
+        {
+            int n = Source.Length;
+            int m = Target.Length;
+
+            Matrix = new int[n + 1, m + 1]; // Hesaplama matrisi üretilir. 2 boyutlu matrisin boyut uzunlukları ise kaynak ve hedef metinlerin karakter uzunluklarına göre set edilir
+
+            if (n == 0) // Eğer kaynak metin yoksa zaten hedef metnin tüm harflerinin değişimi söz konusu olduğundan, hedef metnin uzunluğu kadar bir yakınlık değeri mümkün olabilir 
+                return m;
+
+            if (m == 0) // Yukarıdaki durum hedefin karakter içermemesi halinde de geçerlidir 
+                return n;
+
+            // Aşağıdaki iki döngü ile yatay ve düşey eksenlerdeki standart 0,1,2,3,4...n elemanları doldurulur 
+            for (int i = 0; i <= n; i++)
+                Matrix[i, 0] = i;
+
+            for (int j = 0; j <= m; j++)
+                Matrix[0, j] = j;
+
+            // Kıyaslama ve derecelendirme operasyonu yapılır 
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (Target[j - 1] == Source[i - 1]) ? 0 : 1;
+                    Matrix[i, j] = Math.Min(Math.Min(Matrix[i - 1, j] + 1, Matrix[i, j - 1] + 1), Matrix[i - 1, j - 1] + cost);
+                }
+
+            return Matrix[n, m]; // sağ alt taraftaki hücre değeri döndürülür 
+        }
+    }
+}
